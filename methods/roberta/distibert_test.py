e26f2c366bbf11fdb7c06b1c56eda979054d67ef
PaulRaid <72143205+PaulRaid@users.noreply.github.com>
2022-03-12 10:15:31 +0100

    Working Roberta, organisation

--- methods/roberta/distibert_test.py
+++ methods/roberta/distibert_test.py
@@ -6,6 +6,7 @@ from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
 from transformers import EarlyStoppingCallback
+import csv
 
 torch.cuda.empty_cache()
 
@@ -14,19 +15,11 @@ dataset = pd.read_json('../raw_data/train_set.json')
 
 dataset = dataset[['summary', 'label']]
 
-tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased", use_fast = True)
-model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels = 2)
+model_type = 'distilroberta-base'
+#model_type ='distilbert-base-cased'
 
+tokenizer = AutoTokenizer.from_pretrained(model_type, use_fast = True)
 
-# ----- 1. Preprocess data -----#
-# Preprocess data
-X = list(dataset["summary"])
-y = list(dataset["label"])
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.1)
-X_train_tokenized = tokenizer(X_train, padding=True, truncation=True, max_length=512)
-X_val_tokenized = tokenizer(X_val, padding=True, truncation=True, max_length=512)
-
-# Create torch dataset
 class Dataset(torch.utils.data.Dataset):
     def __init__(self, encodings, labels=None):
         self.encodings = encodings
@@ -40,6 +33,19 @@ class Dataset(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.encodings["input_ids"])
+
+model = AutoModelForSequenceClassification.from_pretrained(model_type, num_labels = 2)
+
+
+
+# ----- 1. Preprocess data -----#
+# Preprocess data
+X = list(dataset["summary"])
+y = list(dataset["label"])
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.01)
+X_train_tokenized = tokenizer(X_train, padding=True, truncation=True, max_length=512)
+X_val_tokenized = tokenizer(X_val, padding=True, truncation=True, max_length=512)
+
 
 train_dataset = Dataset(X_train_tokenized, y_train)
 val_dataset = Dataset(X_val_tokenized, y_val)
@@ -59,14 +65,16 @@ def compute_metrics(p):
 
 # Define Trainer
 args = TrainingArguments(
-    output_dir="output",
+    output_dir="output_bart",
+    overwrite_output_dir = True,
     evaluation_strategy="steps",
     eval_steps=500,
     per_device_train_batch_size=8,
     per_device_eval_batch_size=8,
-    num_train_epochs=3,
+    num_train_epochs=5,
     seed=0,
     load_best_model_at_end=True,
+    save_steps= 1000
 )
 
 
@@ -93,8 +101,8 @@ X_test_tokenized = tokenizer(X_test, padding=True, truncation=True, max_length=5
 test_dataset = Dataset(X_test_tokenized)
 
 # Load trained model
-model_path = "output/checkpoint-50000"
-model = BertForSequenceClassification.from_pretrained(model_path, num_labels=2)
+model_path = "output_bart/checkpoint-2250"
+#model = AutoModelForSequenceClassification.from_pretrained(model_path, num_labels=2)
 
 # Define test trainer
 test_trainer = Trainer(model)
@@ -104,3 +112,9 @@ raw_pred, _, _ = test_trainer.predict(test_dataset)
 
 # Preprocess raw predictions
 y_pred = np.argmax(raw_pred, axis=1)
+
+with open("distibert/submission.csv", "w") as pred:
+    csv_out = csv.writer(pred)
+    csv_out.writerow(['id','label'])
+    for i, row in enumerate(y_pred):
+        csv_out.writerow([i, row])
